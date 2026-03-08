e7d93e4987c9594c29278f0aed2f2fcf5cec3add
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-08 05:39:29 +0000

    Test capitalize 'coding' to be Coding Test result => failed || "RED"

--- capitalize.test.js
+++ capitalize.test.js
@@ -2,3 +2,6 @@ const capitalize = require("./capitalize");
 
 test(`capitalize the word "hamza"`, () =>
   expect(capitalize("hamza")).toBe("Hamza"));
+
+test(`capitalize the word "coding"`, () =>
+  expect(capitalize("coding")).toBe("Coding"));
