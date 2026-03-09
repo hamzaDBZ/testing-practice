a395a4972542471a5fa9130973976992dfc0834a
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 17:47:34 +0000

    Add 2 tests run tests => both failed "RED"

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,5 @@
 const calculator = require("./calculator");
 
 test("add 10 + 5", () => expect(calculator.add(10, 5)).toBe(15));
+test("add 100 + 50", () => expect(calculator.add(100, 50)).toBe(150));
+test("add 66 + 33", () => expect(calculator.add(66, 33)).toBe(99));
