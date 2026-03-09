15f945e8f367219ae42531ff6d475b6ac9e6d3f8
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 06:16:20 +0000

    Add test for boolean run tests => 1 failed "RED"

--- reverseString.test.js
+++ reverseString.test.js
@@ -2,3 +2,4 @@ const reverseString = require("./reverseString");
 
 test(`reverse "hamza"`, () => expect(reverseString("hamza")).toBe("azmah"));
 test(`reverse 56636 number`, () => expect(reverseString(56636)).toBe("63665"));
+test(`reverse boolean`, () => expect(reverseString(false)).toBe("eslaf"));
