eea5c869920fe9c05aa48f273235a9f4f112a808
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 18:02:47 +0000

    Refactor "add" func to handle non-number inputs run tests => all passed "GREEN"

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 function calculator() {
-  const add = (a, b) => a + b;
+  const message = "Input must be valid number";
+  const add = (a, b) =>
+    typeof a === "number" && typeof b === "number" ? a + b : message;
   return { add };
 }
 module.exports = calculator();
