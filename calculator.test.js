0f794dbfc2166dd8f8f52ae3cbec4169bad1ebd4
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 18:12:11 +0000

    Add subtract tests similar to "add" tests run tests => all subtract tests failed "RED"

--- calculator.test.js
+++ calculator.test.js
@@ -9,3 +9,15 @@ test("add 10 + undefined", () =>
   expect(calculator.add(10, undefined)).toBe("Input must be valid number"));
 test("add 10 + Hello", () =>
   expect(calculator.add(10, "Hello")).toBe("Input must be valid number"));
+
+test("subtract 10 - 5", () => expect(calculator.subtract(10, 5)).toBe(5));
+test("subtract 100 - 50", () => expect(calculator.subtract(100, 50)).toBe(10));
+test("subtract 66 - 33", () => expect(calculator.subtract(66, 33)).toBe(33));
+test("subtract 10 - true", () =>
+  expect(calculator.subtract(10, true)).toBe("Input must be valid number"));
+test("subtract 10 - undefined", () =>
+  expect(calculator.subtract(10, undefined)).toBe(
+    "Input must be valid number",
+  ));
+test("subtract 10 - Hello", () =>
+  expect(calculator.subtract(10, "Hello")).toBe("Input must be valid number"));
