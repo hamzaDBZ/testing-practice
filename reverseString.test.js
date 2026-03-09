7a9558b42c8c1b872034d65f6deee29b6af5fb95
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 06:41:36 +0000

    Add 3 more test and RUN => all test passed "GREEN"

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,10 @@ const reverseString = require("./reverseString");
 test(`reverse "hamza"`, () => expect(reverseString("hamza")).toBe("azmah"));
 test(`reverse 56636 number`, () => expect(reverseString(56636)).toBe("63665"));
 test(`reverse boolean`, () => expect(reverseString(false)).toBe("eslaf"));
+test(`reverse boolean 2`, () => expect(reverseString(true)).toBe("eurt"));
+test(`reverse with spaces 1`, () =>
+  expect(reverseString("Ham Za Cod ING")).toBe("GNI doC aZ maH"));
+test(`reverse with spaces 2`, () =>
+  expect(reverseString("    odin Programming   ")).toBe(
+    "   gnimmargorP nido    ",
+  ));
