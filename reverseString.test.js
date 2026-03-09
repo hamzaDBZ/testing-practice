ec15bc81d5c9794a8f2db627e461241528689e81
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-09 06:05:19 +0000

    Add new test 56636 run tests => failed "RED"

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,3 +1,4 @@
 const reverseString = require("./reverseString");
 
 test(`reverse "hamza"`, () => expect(reverseString("hamza")).toBe("azmah"));
+test(`reverse 56636 number`, () => expect(reverseString(56636)).toBe("63665"));
