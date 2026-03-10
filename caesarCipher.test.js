2ffa71fe079029bc444e49c54553985dd8aa322b
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-10 07:11:42 +0000

    Test case preservation => all tests passed "GREEN"

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,4 +1,10 @@
 const caesarCipher = require("./caesarCipher");
 
-test("test hello world with 3 shift factor", () =>
+test("test hello world! with 3 shift factor", () =>
   expect(caesarCipher("hello world!", 3)).toBe("khoor zruog!"));
+
+test("test Hello World! with 3 shift factor", () =>
+  expect(caesarCipher("Hello World!", 3)).toBe("Khoor Zruog!"));
+
+test("test HeLLo with 3 shift factor", () =>
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr"));
