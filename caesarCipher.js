fcf2c303d6eec2b4c7bba7ce332ff442fc5e69fe
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-10 06:59:18 +0000

    Refactor caesarCipher function run first test => passed "GREEN"

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,2 +1,17 @@
-function caesarCipher(string, shiftFactor = 0) {}
+function caesarCipher(string, shiftFactor = 0) {
+  let shifted = "";
+  // let charCode;
+  for (let i = 0; i < string.length; i++) {
+    let charCode = string.charCodeAt(i);
+    if (
+      (charCode >= 65 && charCode <= 90) ||
+      (charCode >= 97 && charCode <= 122)
+    ) {
+      shifted += String.fromCharCode(charCode + shiftFactor);
+    } else {
+      shifted += string[i];
+    }
+  }
+  return shifted;
+}
 module.exports = caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,4 +1,4 @@
 const caesarCipher = require("./caesarCipher");
 
 test("test hello world with 3 shift factor", () =>
-  expect(caesarCipher("hello world", 3)).toBe("khoor zruog"));
+  expect(caesarCipher("hello world!", 3)).toBe("khoor zruog!"));
