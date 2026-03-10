48cada6888a0e56a815c42c8325eb666e0454545
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-10 17:12:59 +0000

    Test large shift factor => all test passed "GREEN"

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,17 +1,36 @@
+function getShiftedCharCode(charCode, shiftFactor) {
+  let shiftedCharCode = charCode + shiftFactor;
+
+  if (charCode > 90) {
+    while (shiftedCharCode > 122) {
+      shiftedCharCode -= 26;
+    }
+    return shiftedCharCode;
+  }
+
+  if (charCode <= 90) {
+    while (shiftedCharCode > 90) {
+      shiftedCharCode -= 26;
+    }
+    return shiftedCharCode;
+  }
+  return shiftedCharCode;
+}
+
 function caesarCipher(string, shiftFactor = 0) {
   let shifted = "";
-  // let charCode;
   for (let i = 0; i < string.length; i++) {
     let charCode = string.charCodeAt(i);
     if (
       (charCode >= 65 && charCode <= 90) ||
       (charCode >= 97 && charCode <= 122)
     ) {
-      shifted += String.fromCharCode(charCode + shiftFactor);
+      shifted += String.fromCharCode(getShiftedCharCode(charCode, shiftFactor));
     } else {
       shifted += string[i];
     }
   }
   return shifted;
 }
+
 module.exports = caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -14,3 +14,21 @@ test("test non-alphabetical characters with 3 shift factor", () =>
 
 test("test non-alphabetical characters with 3 shift factor", () =>
   expect(caesarCipher("   Hello, World!  ", 3)).toBe("   Khoor, Zruog!  "));
+
+test("test large shift factor 100", () =>
+  expect(caesarCipher("   Hello, World!  ", 100)).toBe("   Dahhk, Sknhz!  "));
+
+test("test lowercase alphabet with large shift factor 100", () =>
+  expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 100)).toBe(
+    "wxyzabcdefghijklmnopqrstuv",
+  ));
+
+test("test uppercase alphabet with large shift factor 100", () =>
+  expect(caesarCipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 100)).toBe(
+    "WXYZABCDEFGHIJKLMNOPQRSTUV",
+  ));
+
+test("test mixed case alphabet with large shift factor 100", () =>
+  expect(caesarCipher("abcdEFghiJKLMNOPQRStuvwXYZ", 100)).toBe(
+    "wxyzABcdeFGHIJKLMNOpqrsTUV",
+  ));
