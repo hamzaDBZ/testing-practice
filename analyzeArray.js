21ba291cdf5f7137be05418a00b7cf2dedb14e13
Hamza Rezzouq <razzoukhamza184@gmail.com>
2026-03-11 05:45:31 +0000

    Add first test case => test passed "GREEN" simply return the matching object

---
+++ analyzeArray.js
@@ -0,0 +1,10 @@
+function analyzeArray(array) {
+  return {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  };
+}
+
+module.exports = analyzeArray;
---
+++ analyzeArray.test.js
@@ -0,0 +1,9 @@
+const analyzeArray = require("./analyzeArray");
+
+test("array of numbers", () =>
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  }));
